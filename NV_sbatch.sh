404c716e79db66c0b3c7b8822c6605bd836a16a3
xiangyu han <xiangyu.han@outlook.com>
2025-07-10 10:20:24 -0400

    update batch process with ref

--- NV_sbatch.sh
+++ NV_sbatch.sh
@@ -1,11 +1,11 @@
 
 job_name="3DfoundationModelDifix3D"
-base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/logs/Difix3D_spatial05_frames6000_with_multi_frame_depth"
+base_logdir="/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/logs/spatial05_frames3000_with_multi_frame_depth_horizontal_sine_difix3d_w_ref"
 
 
 # Train
 
-submit_job --gpu 1 --cpu 16 --nodes 1 --partition=grizzly,polar,polar3,polar4 --account=nvr_av_end2endav \
+submit_job --gpu 1 --cpu 64 --nodes 1 --partition=grizzly,polar,polar3,polar4 --account=nvr_av_end2endav \
                 --image=/lustre/fsw/portfolios/nvr/users/ymingli/dockers/cu118.sqsh  \
                 --mounts=/lustre/:/lustre/,/lustre/fsw/portfolios/nvr/users/ymingli/miniconda3:/home/ymingli/miniconda3 \
                 --duration 4 \
