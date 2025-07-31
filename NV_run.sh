7de91a909046b398c45a9654f9382067a7acd2ed
xiangyu han <xiangyu.han@outlook.com>
2025-07-31 20:57:20 +0800

    update batched process w ref

--- NV_run.sh
+++ NV_run.sh
@@ -9,7 +9,7 @@ export PYTHONNOUSERSITE=1
 # Clear PYTHONPATH and  LD_LIBRARY_PATH
 unset PYTHONPATH
 export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64:/usr/lib/x86_64-linux-gnu
-
+video_output_path=/lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4_parallel_left_difix3d_w_ref.mp4
 
 cd /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/Difix3D
 
@@ -19,12 +19,13 @@ cd /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/code/Difix3D
 #   --output_folder /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4/parallel_left/ours_199993/renders_difix_w_ref \
 #   --prompt "remove degradation"
 
-python batched_process_w_ref.py \
-  --input_folder /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4/parallel_right/ours_199993/renders \
-  --ref_folder /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4/train/ours_199993/gt \
-  --output_folder /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4/parallel_right/ours_199993/renders_difix_w_ref \
-  --prompt "remove degradation"
+# python batched_process_w_ref.py \
+#   --input_folder /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4/parallel_right/ours_199993/renders \
+#   --ref_folder /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4/train/ours_199993/gt \
+#   --output_folder /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4/parallel_right/ours_199993/renders_difix_w_ref \
+#   --prompt "remove degradation"
 
-# python render_video_difix_horizon_sine.py /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth/ --fps 15
+python render_video_difix_parallel.py /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth_cap3M_L1depthReg05_randomBG_PostOptlr1e4/ --fps 15
 
+rclone copy "${video_output_path}"  "xiangyuDrive:Research/CityGS/RenderVideos/" -P
 # rclone copy /lustre/fsw/portfolios/nvr/users/ymingli/gaussian/model/spatial05_frames3000_with_multi_frame_depth/spatial05_frames3000_with_multi_frame_depth_horizontal_sine_video_difix_wo_ref.mp4  "xiangyuDrive:Research/CityGS/RenderVideos/" -P
